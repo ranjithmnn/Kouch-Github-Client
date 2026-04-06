ec501ab61ba8c5a4a0245812df9f82ab1fd23fed
Ranjith Menon <ranjithmenondec@gmail.com>
2026-04-06 11:46:04 +0530

    Initial Commit

---
+++ Kouch/KouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  KouchApp.swift
+//  Kouch
+//
+//  Created by Ranjith Menon on 06/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct KouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
