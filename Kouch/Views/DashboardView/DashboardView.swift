81e3feebe0ca8a4891956d0131cc0677568cdd6c
Ranjith Menon <ranjithmenondec@gmail.com>
2026-04-06 11:58:20 +0530

    Initial

---
+++ Kouch/Views/DashboardView/DashboardView.swift
@@ -0,0 +1,19 @@
+//
+//  DashboardView.swift
+//  Kouch
+//
+//  Created by Ranjith Menon on 06/04/2026.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+//    let apiKey = Bundle.main.object(forInfoDictionaryKey: "GITHUB_API_KEY") as? String
+    var body: some View {
+//        Text(apiKey ?? "None")
+    }
+}
+
+#Preview {
+    DashboardView()
+}
